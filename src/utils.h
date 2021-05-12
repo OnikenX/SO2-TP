2c8f3ba66c5a43bd6e2a9748d55a7247c28117ea
João Gonçalves <joao.goncalves@nerdmonkeys.pt>
2021-05-12 19:48:00 +0100

    mais ficheiros no gitignore

--- src/utils.h
+++ src/utils.h
@@ -67,6 +67,9 @@ namespace Wrappers
 
 		~Handle()
 		{
+#ifdef _DEBUG
+			tcerr << t("Closing the handle") << h << std::endl;
+#endif
 			CloseHandle(h);
 			//_tprintf(TEXT("Closed handle"));
 		}
@@ -91,6 +94,9 @@ namespace Wrappers
 		}*/
 		~Handle()
 		{
+#ifdef _DEBUG
+			tcerr << t("Closing the handle") << h << std::endl;
+#endif
 			FreeLibrary(h);
 			//_tprintf(TEXT("Closed handle"));
 		}
@@ -103,124 +109,6 @@ namespace Wrappers
 			return h;
 		}
 	};
-
-	class test
-	{
-		int a;
-
-	public:
-		test() {}
-
-		~test() {}
-
-		int copy()
-		{
-			return a;
-		}
-
-		int &operator()()
-		{
-			return a;
-		}
-	};
-
-	template <typename T>
-	class ValueGuard
-	{
-		T &value;
-		HANDLE mutex;
-		friend ValueGuard;
-
-	public:
-		ValueGuard(HANDLE mutex, T &value) : value(value), mutex(mutex)
-		{
-			tcout << t("geting lock\n");
-			auto switcher = WaitForSingleObject(mutex, INFINITE);
-			tcout << t("waited\n");
-			switch (switcher)
-			{
-			case WAIT_ABANDONED:
-				throw std::exception("Problem in creating guard: WAIT_ABANDONED!");
-				break;
-			case WAIT_OBJECT_0:
-				// SUCCESS
-				break;
-			case WAIT_TIMEOUT:
-				throw std::exception("Problem in creating guard: WAIT_TIMEOUT!");
-				break;
-			case WAIT_FAILED:
-				throw std::exception("Problem in creating guard: WAIT_FAILED!");
-				break;
-			default:
-				std::ostringstream stream;
-				stream << "Problem in creating guard: UNSPECIFIED" << switcher;
-				throw std::exception(stream.str().c_str());
-				break;
-			}
-		}
-
-		~ValueGuard()
-		{
-			ReleaseMutex(mutex);
-		}
-
-		T &get()
-		{
-			return value;
-		}
-	};
-
-	template <typename T>
-	class ValueLocked
-	{
-		T value{};
-		Wrappers::Handle<HANDLE> mutex;
-
-	public:
-		Wrappers::Handle<HANDLE> create_internal_mutex()
-		{
-			static auto name = t("000000");
-
-			auto tmp_mutex = CreateMutex(NULL, TRUE, NULL);
-
-			if (tmp_mutex == NULL)
-				throw std::exception("Error creating mutex.");
-			return Wrappers::Handle<HANDLE>{tmp_mutex};
-		}
-		ValueLocked(T value) : value(value)
-		{
-			mutex = create_internal_mutex();
-		}
-
-		//locks the mutex
-		// template <typename X>
-		ValueGuard<T> lock()
-		{
-			return ValueGuard<T>(mutex(), value);
-		}
-	};
-};
-
-// this is an helper class for printing utf-8 in the windows terminal
-//
-// put these lines in the main and there you go,
-// ```cpp
-//  #ifdef _WIN32
-//      SetConsoleOutputCP(CP_UTF8);
-//      setvbuf(stdout, nullptr, _IONBF, 0);
-//      MBuf buf;
-//      std::cout.rdbuf(&buf);
-//  #endif
-// ```
-class MBuf : public std::stringbuf
-{
-public:
-	int sync()
-	{
-		fputs(str().c_str(), stdout);
-		str("");
-		return 0;
-	}
 };
 
 // corre isto no inicio do programa
