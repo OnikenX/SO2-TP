c91d32d600b10437b11f2dfe3708215e82c7152e
MidgetSlayer <njfs13njfs@gmail.com>
2021-05-13 15:47:19 +0100

    Argv + Structs criadas
    Co-authored-by: OnikenX <OnikenX@pm.me>

--- src/aviao.cpp
+++ src/aviao.cpp
@@ -7,17 +7,41 @@
 #include "utils.h"
 
 
-int _tmain(){
+
+
+int _tmain(int argc, TCHAR *argv[]){
+    if(argc!=4){
+        return ERRO_ARGUMENTOS;
+    }
+
+    aviao a;
+    a.CapMax = (int)argv[1];
+    a.velocidade = (int)argv[2];
+    a.IDAv = (int)argv[3];
+
     
 auto ControlRunning = Wrappers::Handle<HANDLE>(CreateMutex(0, 0,  MUnique));
-
+ 
 //verifica se 
 if(GetLastError() == ERROR_SUCCESS){
     _tprintf(t("Control não iniciado."));
     return 1;
 }
+TCHAR* fileViewMap;
+HANDLE hFileMap;
+ hFileMap = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 100 * sizeof(TCHAR), TEXT("SO2_MEMORIA_PARTILHADA"));
+    if (hFileMap == NULL) {
+        _tprintf(TEXT("Erro no CreateFileMapping\n"));
+        return -1;
+    }
 
 
+fileViewMap = (TCHAR*)MapViewOfFile(hFileMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
+    if (fileViewMap == NULL) {
+        _tprintf(TEXT("Erro no MapViewOfFile\n"));
+        return -1;
+    }
 
     //WaitForSingleObject(hSemaforo, INFINITE);
 return 0;    
