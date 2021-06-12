d2f6fa36cf62ab4915e6d4e2ea2618730535cff7
MidgetSlayer <njfs13njfs@gmail.com>
2021-06-12 13:39:58 +0100

    GUI inicio

---
+++ src/control/GUI/resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ControlerGrafico.rc
+//
+#define IDC_MYICON                      2
+#define IDD_CONTROLERGRAFICO_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDC_POINTER                     104
+#define IDM_EXIT                        105
+#define IDI_CONTROLERGRAFICO            107
+#define IDI_SMALL                       108
+#define IDC_CONTROLERGRAFICO            109
+#define IDR_MAINFRAME                   128
+#define IDD_NovoAviao                   129
+#define IDC_EDIT1                       1003
+#define IDC_NomeAero                    1003
+#define IDC_XCords                      1005
+#define IDC_YCords                      1006
+#define IDC_TXCords                     1007
+#define IDC_TYcords                     1008
+#define IDC_TNomeAero                   1009
+#define ID_FILE_CIARAEROPORTO           32771
+#define ID_FILE_CONSULTAR               32772
+#define ID_CONSULTAR_AEROPORTO          32773
+#define ID_CONSULTAR_AVIAO              32774
+#define ID_CONSULTAR_PASSAGEIROS        32775
+#define ID_FILE_DESATIVARNOVOSAVI32776  32776
+#define ID_FILE_                        32777
+#define IDM_NO_PLANES                   32778
+#define IDM_CRIARAEROPORTO              32779
+#define IDM_Consulta_Aeroporto          32780
+#define IDM_Consulta_Aviao              32781
+#define IDM_Consulta_Passageiros        32782
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
